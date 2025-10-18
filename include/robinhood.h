577f5f236b7431863343ded6b5b3c7d98db2ab91
Ramantan21 <sdi1800281@di.uoa.gr>
2025-10-18 13:22:10 +0300

    .

--- include/robinhood.h
+++ include/robinhood.h
@@ -28,13 +28,14 @@ public:
         size_t p = hash(key) % size;
         size_t vpsl = 0;
         Key k = key;
-        size_t steps = 0;
         std::vector<size_t> v{idx};
+        //check if the key already exists
         while(table[p].has_value()){
             if(table[p]->key == key){
                 table[p]->indices.push_back(idx);
                 return;
             }
+            //if entry has smaller psl swap
             if(vpsl > table[p]->psl){
                 std::swap(k,table[p]->key);
                 std::swap(v,table[p]->indices);
@@ -42,12 +43,8 @@ public:
             }
             p = (p+1) % size;
             vpsl++;
-            steps++;
-            if(steps >= size){
-                throw std::runtime_error("RobinHoodTable is full");
-            }
         }
-        table[p] = Entry<Key>{k,vpsl,v};
+        table[p] = Entry<Key>{k,vpsl,std::move(v)};
     }
 
     std::optional<std::vector<size_t>*>search(const Key& key){
@@ -57,7 +54,7 @@ public:
             if(table[p]->key == key){
                 return &table[p]->indices;
             }
-            if(table[p]->psl < vpsl){
+            if(vpsl > table[p]->psl){
                 return std::nullopt;
             }
             p = (p + 1) % size;
