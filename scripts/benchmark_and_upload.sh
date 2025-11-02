ddbb98cde84b2c37e881f1f8f97f130b17c55632
themos <themos360@gmail.com>
2025-11-03 01:06:23 +0200

    adding network isolation to scripts

--- scripts/benchmark_and_upload.sh
+++ scripts/benchmark_and_upload.sh
@@ -4,12 +4,16 @@ sys=$(uname -s)
 
 if [[ $sys == Linux ]]; then
     echo "Sudo password is required to prevent system from sleeping during benchmarks"
+    sudo nmcli networking off
     sudo systemd-inhibit python3 scripts/benchmark.py 
+    sudo nmcli networking on
 elif [[ $sys == Darwin ]]; then
-    caffeinate &
-    python3 scripts/benchmark.py
-    pkill caffeinate
+    networksetup -setairportpower en0 off
+    caffeinate python3 scripts/benchmark.py
+    networksetup -setairportpower en0 on
 fi
+
+sleep 5
 
 cd ../project-site 
 
