b17682837a256edeae336e3778065e45d47e55cf
themosgit <themos360@gmail.com>
2026-01-26 00:32:25 +0200

    fix: workflow to use ubuntu-latest runner

--- include/platform/hardware.h
+++ include/platform/hardware.h
@@ -1,19 +1,18 @@
 /**
- * @file hardware.h
- * @brief Default hardware configuration constants.
  *
- * Fallback for generic x86-64 Linux when neither Darwin nor benchmarkvm
- * defined.
+ * @file hardware.h
  *
  * @see hardware_darwin.h for Apple Silicon.
- * @see hardware_benchmarkvm.h for contest VM.
- */
+ *
+ **/
 #pragma once
 
-#define SPC__CORE_COUNT 8
-#define SPC__THREAD_COUNT 16
+#define SPC__CORE_COUNT 6
+#define SPC__THREAD_COUNT 6
 #define SPC__LEVEL1_DCACHE_SIZE 32768
 #define SPC__LEVEL2_CACHE_SIZE 1048576
 #define SPC__LEVEL3_CACHE_SIZE 33554432
 #define SPC__LEVEL1_DCACHE_LINESIZE 64
-#define SPC__NUMA_NODE_DRAM_MB 16384
+#ifndef SPC__NUMA_NODE_DRAM_MB
+    #define SPC__NUMA_NODE_DRAM_MB 16384
+#endif
